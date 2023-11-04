766665f8d6f2c0d8bf0ef4b9abfc405922a9004b
THE_EXP <105677206+THE-EXP@users.noreply.github.com>
2023-11-04 14:37:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,46 +10,47 @@ const mainPort = process.env.MAIN_SERVER_PORT || 8080; //* get app port from the
 const table = process.env.USER_TABLE || 'test_table';
 const pool = new pg.Pool();
 // const client = new pg.Client();
-async function init(pool) {
+async function init() {
     pool.query(
         `create table if not exists ${table} (
-            UUID SERIAL PRIMARY KEY,
+            UUID serial,
             EMAIL TEXT NOT NULL,
-            USERNAME TEXT NOT NULL
+            USERNAME TEXT NOT NULL,
+            PRIMARY KEY (UUID)
         );`
     );
 }
-init(pool);
-app.post('/api/addUser', (req, res) =>{addUser(req, res, pool)});
-app.put('/api/editUser', (req, res) =>{editUser(req, res, pool)});
-app.get('/api/query', (req, res) =>{queryUsers(req, res, pool)});
+init();
+app.post('/api/addUser', (req, res) =>{addUser(req, res)});
+app.put('/api/editUser', (req, res) =>{editUser(req, res)});
+app.get('/api/query', (req, res) =>{queryUsers(req, res)});
 
 app.listen(mainPort, ()=>{console.log("Server started on port:", mainPort);}); //start the server on <mainPort> from 9:7
 
 
 
-async function addUser(req, res, pool) {
-    const client = await pool.connect();
+async function addUser(req, res) {
+    //const client = await pool.connect();
     if (req.query != null) {
         var username = req.query.username;
         var email = req.query.email;
         pool.query(`insert into ${table} (USERNAME, EMAIL) values ('${username}', '${email}')`);
         res.send(`user ${username} has been succesfully created`);
     } else if (req.query.username == null) {
-        res.status(400).send(`<username> can not be blank`)
+        res.status(400).send(`<username> can not be blank`);
     } else if (req.query.email == null) {
-        res.status(400).send(`<email> can not be blank`)
+        res.status(400).send(`<email> can not be blank`);
     }
     //client.release();
 }
 
-async function editUser(req, res, pool) {
+async function editUser(req, res) {
     const client = await pool.connect();
 
     client.release();
 }
 
-async function queryUsers(req, res, pool) {
+async function queryUsers(req, res) {
     const client = await pool.connect();
 
     client.release();
