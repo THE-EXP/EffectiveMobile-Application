92eaf402402b742518b1ac17a6577ce7708a9e09
THE_EXP <105677206+THE-EXP@users.noreply.github.com>
2023-11-05 14:24:26 +0300

    update index.js and history.js
    complete codebase for both services, tested and working

--- history.js
+++ history.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config('./');
 const pg = require('pg');
 app.set(express.urlencoded({extended: true}));
+app.use(express.json());
 //* Required Modules
 
 const mainPort = process.env.HISTORY_SERVER_PORT || 8081; //* get app port from the config, otherwise use the default 8081
@@ -10,13 +11,13 @@ const table = process.env.HISTORY_TABLE || 'history_table';
 const pool = new pg.Pool();
 // const client = new pg.Client(); //* connect a client, possibly not needed because we're using Pool from pg
 async function init() {
-    pool.query(
-        `create table if not exists ${table} (
-            Edit-Date TEXT not null,
-            relevantUUID TEXT PRIMARY KEY,
-            entryData TEXT NOT NULL
-        );`
-    );
+  pool.query(
+    `create table if not exists ${table} (
+      EventDate TEXT not null,
+      relevantUUID TEXT not null,
+      entryData TEXT not null
+    );`
+  );
 }
 init();
 app.post('/api/addEntry', (req, res) =>{addEntry(req, res);});
@@ -25,9 +26,45 @@ app.get('/api/query', (req, res) =>{queryEdits(req, res);});
 app.listen(mainPort, ()=>{console.log("Server started on port:", mainPort);}); //start the server on <mainPort> from 10:7
 
 async function addEntry(req,res) {
-    
+  const client = await pool.connect();
+  if(req.body != null) {
+    switch(req.body.action) {
+      case 'CREATE USER':
+        client.query(`insert into ${table} (relevantUUID,EventDate,entryData) values ('${req.body.UUID}','${Date.now()}','${req.body.action + ' with username: ' + req.body.username + ' and email: ' + req.body.email}')`);
+				console.log(`Adding CREATE event for user ${req.body.UUID}`);
+        res.send('OK');
+        break;
+      case 'EDIT USERNAME':
+        client.query(`insert into ${table} (relevantUUID,EventDate,entryData) values ('${req.body.UUID}','${Date.now()}','${req.body.action + ' ' + req.body.old_username + ' -> ' + req.body.new_username}')`);
+				console.log(`Adding EDIT event for user ${req.body.UUID}`);
+        res.send('OK');
+        break;
+      case 'EDIT EMAIL':
+        client.query(`insert into ${table} (relevantUUID,EventDate,entryData) values ('${req.body.UUID}','${Date.now()}','${req.body.action + ' ' + req.body.old_email + ' -> ' + req.body.new_email}')`);
+				console.log(`Adding EDIT event for user ${req.body.UUID}`);
+				res.send('OK');
+        break;
+			case 'EDIT BOTH':
+				client.query(`insert into ${table} (relevantUUID,EventDate,entryData) values ('${req.body.UUID}','${Date.now()}','${req.body.action + ' ' + req.body.old_email + ' -> ' + req.body.new_email}, ${req.body.old_username + ' -> ' + req.body.new_username}')`);
+				console.log(`Adding EDIT event for user ${req.body.UUID}`);
+				res.send('OK');
+        break;
+      default:
+        res.send('OK');
+				break;
+    }
+  client.release();
+  }
 }
 
 async function queryEdits(req,res) {
-
+  const client = await pool.connect();
+  if (req.query.uuid != null) {
+		var edits = await client.query(`select * from ${table} where uuid = ${req.query.uuid}`);
+		res.json(JSON.stringify(edits.rows));
+  } else {
+		var edits = await client.query(`select * from ${table}`);
+		res.json(edits.rows);
+	}
+  client.release();
 }
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
+const axios = require('axios'); //* currently easy to use and test, changing to MQTT exchange might be more foolproof and secure thatn current design
 require('dotenv').config('./');
 const crypto = require('crypto');
 const pg = require('pg');
 app.set(express.urlencoded({extended: true}));
+app.use(express.json());
 //* Required Modules
 
 const mainPort = process.env.MAIN_SERVER_PORT || 8080; //* get app port from the config, otherwise use the default 8080
@@ -15,8 +16,8 @@ async function init() {
   pool.query(
     `create table if not exists ${table} (
       UUID TEXT,
-      MAIL TEXT NOT NULL,
       USERNAME TEXT NOT NULL,
+      EMAIL TEXT NOT NULL,
       PRIMARY KEY (UUID)
       );`
   );
@@ -36,8 +37,9 @@ async function addUser(req, res) {
     var username = req.query.username;
     var email = req.query.email;
 		var randomUUID = crypto.randomUUID();
+    console.log(username, email, randomUUID);
     var result = await client.query(`select * from ${table} where username='${username}' or email='${email}'`);
-    if (result == null) {
+    if (result.rows[0] == null) {
       var result = await client.query(`insert into ${table} (USERNAME, EMAIL, UUID) values ('${username}', '${email}', '${randomUUID}')`);
 			console.log(await axios.request({
 				method: 'POST',
@@ -50,7 +52,7 @@ async function addUser(req, res) {
 					email: email
 				}
 			}).then((response) => {console.log(JSON.stringify(response.data))}).catch((err) =>{if(err){console.error(err);}}));
-      res.send(`user ${username} has been succesfully created\n${JSON.stringify(result)}`);
+      res.send(`user ${username} has been succesfully created\nUser's UUID is ${randomUUID}`);
       } else {
         res.send(`User with this username and/or email already exists!\nPlease chose another username and/or use different email address`);
       }
@@ -71,11 +73,49 @@ async function editUser(req, res) {
     var user = await client.query(`select * from ${table} where username = '${change}'`);
     if (user == null) {
       res.send(`No user with provided username(${change}) exists!`)
-    } else if (new_email != null) {
+    } else if (new_username != null && new_email != null){
+      client.query(`update ${table} set username = '${new_username}', email = '${new_email}' where uuid = '${user.rows[0].uuid}'`);
+      console.log(await axios.request({
+				method: 'POST',
+				baseURL: 'http://localhost:8081/api/',
+				url: `/addEntry`,
+				data: {
+					UUID: user.rows[0].uuid,
+					action: 'EDIT BOTH',
+          old_email: user.rows[0].email,
+          new_email: new_email,
+					old_username: user.rows[0].username,
+          new_username: new_username
+				}
+			}).then((response) => {console.log(JSON.stringify(response.data))}).catch((err) =>{if(err){console.error(err);}}));
+      res.send(`New username for user ${change} is now ${new_username}, and new email is ${new_email}`);
+    } else if (new_username == null) {
       client.query(`update ${table} set email = '${new_email}' where uuid = '${user.rows[0].uuid}'`);
+      console.log(await axios.request({
+				method: 'POST',
+				baseURL: 'http://localhost:8081/api/',
+				url: `/addEntry`,
+				data: {
+					UUID: user.rows[0].uuid,
+					action: 'EDIT EMAIL',
+					old_email: user.rows[0].email,
+          new_email: new_email
+				}
+			}).then((response) => {console.log(JSON.stringify(response.data))}).catch((err) =>{if(err){console.error(err);}}));
       res.send(`New email for user ${change} is now ${new_email}`);
-    } else if (new_username != null) {
-      client.query(`update ${table} set username = '${new_username}' where uuid = '${user}'`);
+    } else if (new_email == null) {
+      client.query(`update ${table} set username = '${new_username}' where uuid = '${user.rows[0].uuid}'`);
+      console.log(await axios.request({
+				method: 'POST',
+				baseURL: 'http://localhost:8081/api/',
+				url: `/addEntry`,
+				data: {
+					UUID: user.rows[0].uuid,
+					action: 'EDIT USERNAME',
+					old_username: user.rows[0].username,
+          new_username: new_username
+				}
+			}).then((response) => {console.log(JSON.stringify(response.data))}).catch((err) =>{if(err){console.error(err);}}));
       res.send(`New username for user ${change} is now ${new_username}`);
     }
   }
@@ -90,7 +130,7 @@ async function queryUsers(req, res) {
       case 'username':
         if (req.query.username != null){
           var result = await client.query(`select * from ${table} where username = '${req.query.username}'`);
-          res.send(JSON.stringify(result.rows));
+          res.json(result.rows);
         } else {
           res.send(`Username expected, got <Null> instead`);
         }
@@ -106,7 +146,7 @@ async function queryUsers(req, res) {
       case 'uuid':
         if (req.query.username != null){
         	var result = await client.query(`select * from ${table} where uuid = '${req.query.uuid}'`);
-          res.send(JSON.stringify(result.rows));
+          res.json(result.rows);
         } else {
           res.send(`UUID expected, got <Null> instead`);
         }
